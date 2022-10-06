9c23dc5c8d276d599cad87d2e6cb1aa441c59ffb
Maria Belen <belul@hotmail.com>
2022-10-06 21:08:51 +0200

    Actualizacion

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ const data = () =>{
 
 data();
 
-
 const simplify = () =>{
     fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(info => info.json())
@@ -33,3 +32,26 @@ const vectorify = () =>{
 }
 vectorify();
 
+const texto1 = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts/4')
+    .then(info => info.json())
+    .then(texto => document.querySelector('#texto1').innerHTML = texto.body)
+    .catch((err) => console.log(`Ha habido un problema: ${err}`));
+}
+texto1();
+
+const texto2 = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts/5')
+    .then(info => info.json())
+    .then(texto => document.querySelector('#texto2').innerHTML = texto.body)
+    .catch((err) => console.log(`Ha habido un problema: ${err}`));
+}
+texto2();
+
+const texto3 = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts/6')
+    .then(info => info.json())
+    .then(texto => document.querySelector('#texto3').innerHTML = texto.body)
+    .catch((err) => console.log(`Ha habido un problema: ${err}`));
+}
+texto3();
